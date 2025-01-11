8dda90b47598b61dd88d8324944b74b7ee3ac389
anshultiwari95 <anshultiwari95@gmail.com>
2025-01-12 01:08:42 +0530

    Added User Schema

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    hobbies: [{ type: String }], // Array of hobbies
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
